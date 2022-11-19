ef27ef1085de100a8d94b2cd07c8c432433aee1d
Gabriel Leyva Merino <gleyvamerino@gmail.com>
2022-11-18 19:27:00 -0500

    The Gallery Page alignment

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -3,76 +3,50 @@
 import stories from '../assets/story.json';
 
 export default {
-  name: 'MyStory',
-  data() {
-    return {
-      stories: stories,
-    };
-  }
+    name: 'Story',
+    props: ['story', 'index'],
 };
 
 </script>
 
 <template>
-    <div class="info-wrapper">
-      <div class="paragraph"
-           v-for="story, index in stories"
-           :key="story">
-           <div v-if="index % 2 == 1" class="image-section">
-                <img :src="story.image">
-            </div>
-            <div class="info-section">
-                <h1>{{story.title}}</h1>
-                <p>{{story.text}}</p>
-            </div>
-            <div v-if="index % 2 == 0" class="image-section">
-                <img :src="story.image">
-            </div>
-      </div>
+    <div class="story">
+        <div v-if="index % 2 == 1" class="story-image-container">
+            <img :src="story.image">
+        </div>
+        <div class="story-info">
+            <h1>{{ story.title }}</h1>
+            <p>{{ story.text }}</p>
+        </div>
+        <div v-if="index % 2 == 0" class="story-image-container">
+            <img :src="story.image">
+        </div>
     </div>
 </template>
 
 <style>
 
-.info-wrapper {
-    text-align: center;
-    align-items: center;
-    align-items: center;
-    align-items: center;
-    justify-content: center;
-}
-.paragraph {
+.story {
     display: flex;
     text-align: left;
     column-gap: 24px;
     white-space: pre-wrap;
     max-width: 1024px;
-    margin-bottom: 32px;    
+    margin-bottom: 32px;
 }
 
-.paragraph:nth-child(odd) {
-    background-color: var(--main-background-color);
-}
-
-.paragraph:nth-child(even) {
-    background-color: var(--tile-background-color);
-}
-
-.paragraph img {
+.story img {
     height: 100%;
     width: 100%;
     object-fit: cover;
 }
 
-.info-section {
+.story-info {
     flex-grow: 1;
-}
-
-.info-section p {
     padding: 8px;
 }
 
-.image-section {
+.story-image-container {
     flex-grow: 1;
     overflow: hidden;
 }
--- src/views/MyStoryView.vue
+++ src/views/MyStoryView.vue
@@ -1,27 +1,62 @@
 
 <script>
 import Story from '../components/Story.vue'
+import stories from '../assets/story.json';
 
 export default {
   name: 'MyStory',
   components: {
     Story
   },
+  data() {
+    return {
+      stories: stories,
+    };
+  }
 };
 </script>
 
 <template>
-    <div class="appear my-story">
-        <h2>
-            My Story
-        </h2>
-        <div class="my-story">
-            <Story></Story>
-        </div>
+  <div class="appear my-story">
+    <div class="story-title-container">
+      <h2>
+        My Story
+      </h2>
     </div>
+    <div class="stories-container">
+      <div class="main-story-container" v-for="story, index in stories" :key="story">
+        <Story :story="story" :index="index"></Story>
+      </div>
+    </div>
+  </div>
 </template>
   
 <style>
+.my-story {
+  position: relative;
+  top: 0;
+  text-align: center;
+  width: 1024px;
+  display: inline-block;
+  margin: 0 auto;
+  height: 100%;
+}
 
+.story-title-container {
+  padding: 8px;
+}
+
+.stories-container {
+  margin: 0 auto;
+  display: inline-block;
+}
+
+.main-story-container:nth-child(odd) {
+  background-color: var(--main-background-color);
+}
+
+.main-story-container:nth-child(even) {
+  background-color: var(--tile-background-color);
+}
 </style>
   
